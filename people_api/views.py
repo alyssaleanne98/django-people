34eb861b3a7a201d2bb9cf63c311c0cc8a9c1ab4
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-02-11 14:59:23 -0800

    created people serializers

--- people_api/views.py
+++ people_api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework.views import APIView    # <- as super to your class
+from rest_framework.response import Response  # <- to send data to the frontend
+from rest_framework import status # <- to include status codes in your response
+
+from .serializers import PeopleSerializer # <- to format data to and from the database, enforces schema
+
+from .models import People
+
+
+# class (People)
+
+#  GET     /people - index
+#  POST    /people - create
+
+# class  (PeopleDetail) - use primary key (pk) as argument to access id
+
+#  GET     /people/:id - show
+#  PUT     /people/:id - update
+#  DELETE  /people/:id - delete
+
+class persons(APIView):
+
+   def get(self, request):
+    # Index Request
+    print(request)
+    # Get all books from the book table
+    persons = People.objects.all()
+    # Use serializer to format table data to JSON
+    data = PeopleSerializer(persons, many=True).data
+    return Response(data)
